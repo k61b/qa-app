45d0bad85b8563fc90fa3760af5e68ad2d306bb2
Kayra <kayrabtuncer@gmail.com>
2021-02-27 12:56:13 +0300

    Create New User

--- server/src/controllers/auth/auth.controller.ts
+++ server/src/controllers/auth/auth.controller.ts
@@ -1,25 +1,36 @@
 import * as express from 'express'
 import { Request, Response } from 'express'
 import IControllerBase from 'src/interfaces/IControllerBase.interface'
+import IUser from './auth.interface'
+import User from '../../model/user.model'
 
 class AuthController implements IControllerBase {
     public path = '/auth'
     public router = express.Router()
+
+    private users: IUser[] = []
 
     constructor() {
         this.initRoutes()
     }
 
     public initRoutes() {
-        this.router.get(`${this.path}/register`, this.getRegister)
+        this.router.post(`${this.path}/register`, this.createUser)
     }
 
-    getRegister = (req: Request, res: Response) => {
+    createUser = async (req: Request, res: Response) => {
+        const user: IUser = req.body
+        this.users.push(user)
+        
+        const newUser:IUser[] = await User.create(this.users)
+
         res
-            .status(200)
-            .json({
-                success: true
-            })
+        .status(201)
+        .json({
+            success: true,
+            data: newUser
+        });
+
     }
 }
 
